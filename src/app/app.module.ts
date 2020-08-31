85ab3dcb43a521e5e99f6402cef1e413517ed0c2
Delix <mdt.webmaster@gmail.com>
2020-08-31 15:25:02 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
+import { RouterModule } from '@angular/router';
+import { AppRoutingRoutingModule } from './app-routing-routing.module';
+import { UsuariosModule } from './usuarios/usuarios.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { SharedModule } from "./shared/shared.module";
+import { HttpClientModule } from "@angular/common/http";
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingRoutingModule,
+    HttpClientModule,
+    SharedModule,
+    UsuariosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
